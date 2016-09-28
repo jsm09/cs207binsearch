b3bf74bee04e659938d34b35e3b26075e07bc463
jsm09 <lware96@gmail.com>
2016-09-28 17:15:01 -0400

    adding test based on placement of the needle in the list

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # cs207binsearch
 
 [![Build Status](https://travis-ci.org/jsm09/cs207binsearch.svg?branch=master)](https://travis-ci.org/jsm09/cs207binsearch)
-
 [![Coverage Status](https://coveralls.io/repos/github/jsm09/cs207binsearch/badge.svg)](https://coveralls.io/github/jsm09/cs207binsearch)
--- test_binsearch.py
+++ test_binsearch.py
@@ -9,9 +9,23 @@ def test_empty_list():
 def test_muliple_instances():
     assert binary_search([0,3,3,4,5,9],3) in [1,2]
 
+# Tests based on the data types in the array
 def test_nan_in_list():
     assert binary_search([1,float('NaN'),3,5,float('NaN')],2) == 1
 
 def test_inf_in_list():
     assert binary_search([2,float('Inf'),5,7,9],5)==2
+
+# Tests based on the placement of the needle in the array
+def test_needle_above_range():
+    assert binary_search([2,3,6,8,12,34],56) == -1
+
+def test_needle_below_range():
+    assert binary_search([5,7,23,46,68,89],2) == -1
+
+def test_needle_last_index():
+    assert binary_search([2,3,5,7,23,64],64) == 5
+
+def test_needle_first_index():
+    assert binary_search([56,65,71,78],56) == 0
 # py.test --doctest-modules --cov --cov-report term-missing binsearch.py test_binsearch.py
